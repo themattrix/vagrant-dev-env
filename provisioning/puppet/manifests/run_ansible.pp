1ef8e236a5a63de66967b84f7fcdee8caeaec104
Matthew Tardiff <mattrix@gmail.com>
2013-12-10 21:36:32 -0600

    Home configuration is now cloned from github.

--- provisioning/puppet/manifests/run_ansible.pp
+++ provisioning/puppet/manifests/run_ansible.pp
@@ -1,3 +1,45 @@
+# Vagrant user should be able to SSH to localhost without a problem
+
+$vagrant_ssh_dir = '/home/vagrant/.ssh'
+
+file { "$vagrant_ssh_dir":
+  ensure => directory,
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0644',
+} ->
+
+exec { 'generate-local-ssh-key':
+  path    => ['/bin', '/usr/bin'],
+  cwd     => "$vagrant_ssh_dir",
+  user    => 'vagrant',
+  command => 'ssh-keygen -t rsa -b 4096 -N "" -f id_rsa',
+  creates => "$vagrant_ssh_dir/id_rsa",
+} ->
+
+exec { 'authorize-ssh-key':
+  path    => ['/bin', '/usr/bin'],
+  cwd     => "$vagrant_ssh_dir",
+  user    => 'vagrant',
+  command => "cat id_rsa.pub >> authorized_keys",
+  unless  => 'grep -sqFx "$(cat id_rsa.pub)" authorized_keys',
+} ->
+
+file { "$vagrant_ssh_dir/authorized_keys":
+  owner => 'vagrant',
+  group => 'vagrant',
+  mode  => '0400',
+} ->
+
+exec { 'add-localhost-to-known-hosts':
+  path    => ['/bin', '/usr/bin'],
+  cwd     => "$vagrant_ssh_dir",
+  user    => 'vagrant',
+  command => 'ssh-keyscan -t rsa localhost >> known_hosts',
+  unless  => 'grep -sq "^localhost " known_hosts',
+}
+
+
 # Ensure apt database has been updated before any packages are installed
 Exec['apt-get-update'] -> Package<| |>
 
@@ -26,8 +68,12 @@ file { '/etc/ansible/hosts':
 
 exec { 'run-ansible':
   path      => ['/bin', '/usr/bin', '/sbin', '/usr/sbin', '/usr/local/sbin'],
-  command   => 'ansible-playbook /vagrant/provisioning/ansible/playbook.yml --connection=local',
+  user      => 'vagrant',
+  command   => 'ansible-playbook /vagrant/provisioning/ansible/playbook.yml',
   logoutput => true,
   timeout   => 0,
-  require   => File['/etc/ansible/hosts']
+  require   => [
+    File['/etc/ansible/hosts'],
+    Exec['add-localhost-to-known-hosts'],
+  ]
 }
