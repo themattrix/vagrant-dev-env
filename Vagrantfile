6de33f4aecf415c281bd3744885173f29d2c373c
Matthew Tardiff <mattrix@gmail.com>
2013-12-22 20:50:18 +0000

    The clone-home playbook now runs on CentOS

--- Vagrantfile
+++ Vagrantfile
@@ -9,17 +9,17 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.define "ubuntu" do |ubuntu|
         ubuntu.vm.box     = "saucy64"
         ubuntu.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/saucy/current/saucy-server-cloudimg-amd64-vagrant-disk1.box"
-
-        # Initially with Puppet for the sole purpose of installing and running Ansible on the VM.
-        ubuntu.vm.provision "puppet" do |puppet|
-            puppet.manifests_path = "provisioning/puppet/manifests"
-            puppet.manifest_file  = "run_ansible.pp"
-        end
     end
 
     # Define a CentOS 6.4 x64 machine
     config.vm.define "centos" do |centos|
         centos.vm.box     = "centos64"
-        centos.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-64-x64-vbox4210-nocm.box"
+        centos.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-64-x64-vbox4210.box"
+    end
+
+    # Initially with Puppet for the sole purpose of installing and running Ansible on the VM.
+    config.vm.provision "puppet" do |puppet|
+        puppet.manifests_path = "provisioning/puppet/manifests"
+        puppet.manifest_file  = "run_ansible.pp"
     end
 end
--- provisioning/puppet/manifests/run_ansible.pp
+++ provisioning/puppet/manifests/run_ansible.pp
@@ -1,24 +1,39 @@
-# Ensure apt database has been updated before any packages are installed
-Exec['apt-get-update'] -> Package<| |>
-
 Exec {
   path => ['/usr/local/sbin', '/usr/sbin', '/sbin', '/usr/local/bin', '/usr/bin', '/bin'],
 }
 
-exec { 'add-ansible-repo':
-  command   => 'add-apt-repository -y ppa:rquillo/ansible',
-  creates   => '/etc/apt/trusted.gpg.d/rquillo-ansible.gpg',
-  logoutput => true,
-}
+if $operatingsystem == "Ubuntu" {
 
-exec { 'apt-get-update':
-  command => 'apt-get update',
-  require => Exec['add-ansible-repo'],
-  onlyif  => '[ $(($(date +%s) - $(date +%s -d "$(stat -c %y /var/lib/apt/periodic/update-success-stamp)"))) -gt 3600 ]',
+  # Ensure apt database has been updated before any packages are installed
+  Exec['apt-get-update'] -> Package<| |>
+
+  exec { 'add-ansible-repo':
+    command   => 'add-apt-repository -y ppa:rquillo/ansible',
+    creates   => '/etc/apt/trusted.gpg.d/rquillo-ansible.gpg',
+    logoutput => true,
+  }
+
+  exec { 'apt-get-update':
+    command => 'apt-get update',
+    onlyif  => '[ $(($(date +%s) - $(date +%s -d "$(stat -c %y /var/lib/apt/periodic/update-success-stamp)"))) -gt 3600 ]',
+    require => Exec['add-ansible-repo'],
+  }
+
+} elsif $operatingsystem == "CentOS" {
+
+  # Ensure epel repo is installed before any packaged are installed
+  Exec['install-epel'] -> Package<| |>
+
+  $epel_url = 'http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
+
+  exec { 'install-epel':
+    command => "rpm -Uvh $epel_url",
+    unless  => "yum repolist | grep '^epel '",
+  }
 }
 
 package { 'ansible':
-  ensure  => installed,
+  ensure => installed,
 }
 
 file { '/etc/ansible/hosts':
